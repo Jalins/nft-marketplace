b6d22f754e925b2dd95fb3730f167eebb9b03827
Jalins <zenglinhuang@gmail.com>
2022-11-09 21:51:09 +0800

    新增web3 provider

---
+++ components/provider/web3/index.tsx
@@ -0,0 +1,19 @@
+import { createContext, FunctionComponent, useContext, useState } from "react";
+
+const Web3Context = createContext<any>(null);
+
+const Web3Provider: FunctionComponent = ({children}) => {
+    const [web3Api, setWeb3Api] = useState({test: "hello web3!!!!"});
+
+    return (
+        <Web3Context.Provider value={web3Api}>
+            {children}
+        </Web3Context.Provider>
+    )
+}
+
+export function  useWeb3(){
+    return useContext(Web3Context)
+}
+
+export default Web3Provider;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Web3Provider from '@provider/web3'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <Web3Provider>
+        <Component {...pageProps} />
+      </Web3Provider>
     </>
   )
 }
